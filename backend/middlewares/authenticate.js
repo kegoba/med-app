b9fafa3e49db72fce20b36cd2e70d38d76aba395
Egoba Kelvin <egobakelvin@gmail.com>
2024-05-28 21:40:09 +0100

    bug fix on authentication file

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -1,18 +1,28 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { JWT_SECRET } = process.env;
 
 const authenticate = (req, res, next) => {
-  const token = req.header('Authorization')
-  console.log(token)
-  if (!token) {
-    return res.status(401).send('Access denied');
+  const authHeader = req.header('Authorization');
+console.log("process.env.JWT_SECRET")
+  if (!authHeader) {
+    return res.status(401).send('Access denied. No token provided.');
   }
+
+  const token = authHeader.split(' ')[1]; // Extract the token from 'Bearer <token>'
+
+  if (!token) {
+    return res.status(401).send('Access denied. Invalid token format.');
+  }
+   
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);//JWT_SECRET  
     req.officer = decoded;
     next();
   } catch (err) {
-    res.status(400).send('Invalid token');
+    console.error('Token verification failed:', err);
+    res.status(400).send('Invalid token.');
   }
 };
 
-module.exports = authenticate;
+module.exports = authenticate;
--- backend/routes/officer.route.js
+++ backend/routes/officer.route.js
@@ -1,5 +1,5 @@
 const route = require('express').Router()
-
+const authenticate = require('../middlewares/authenticate');
 const officerController = require('../controllers/officer.controller')
 
 
@@ -7,11 +7,11 @@ const officerController = require('../controllers/officer.controller')
 
 
 
-route.post('/registeruser', officerController.registerOfficer)
+route.post('/registeruser',  officerController.registerOfficer)
 route.post('/loginuser', officerController.loginOfficer) 
-route.get('/getalluser', officerController.getAllUser) 
-route.get('/getsingleuser/:id', officerController.getSingleUser)  
-route.get('/deleteuser', officerController.deleteAllUser)  
+route.get('/getalluser',authenticate, officerController.getAllUser) 
+route.get('/getsingleuser/:id', authenticate, officerController.getSingleUser)  
+route.get('/deleteuser', authenticate, officerController.deleteAllUser)  
 
 
 
